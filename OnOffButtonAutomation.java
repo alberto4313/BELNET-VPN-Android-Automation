eecfb951249374b247e31484036ba856d001cb88
alberto4313 <135798204+alberto4313@users.noreply.github.com>
2023-06-08 13:56:25 +0530

    Update OnOffButtonAutomation.java

--- OnOffButtonAutomation.java
+++ OnOffButtonAutomation.java
@@ -13,10 +13,10 @@ public class OnOffButtonAutomation {
         dc.setCapability(MobileCapabilityType.AUTOMATION_NAME , "Appium");
         dc.setCapability(MobileCapabilityType.PLATFORM_NAME , "Android");
         dc.setCapability(MobileCapabilityType.PLATFORM_VERSION , "12");
-        dc.setCapability(MobileCapabilityType.DEVICE_NAME, "ceaae327");
+        dc.setCapability(MobileCapabilityType.DEVICE_NAME, "ceaae327");// This value is device specific open CMD and type adb.exe devices
         dc.setCapability("udid","ceaae327");
-        dc.setCapability("appPackage" , "io.beldex.belnet");
-        dc.setCapability("appActivity" , "io.beldex.belnet.MainActivity");
+        dc.setCapability("appPackage" , "io.beldex.belnet");//appPackage can be found using APKInfo application
+        dc.setCapability("appActivity" , "io.beldex.belnet.MainActivity");//appActivity can be found using APKInfo application
 	     URL d =new URL("http://localhost:4723/wd/hub");
 	    AndroidDriver driver =new AndroidDriver(d,dc);
 		for(int i=0;i<=10;i++) {
